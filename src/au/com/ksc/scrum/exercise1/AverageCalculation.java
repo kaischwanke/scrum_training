6539c9bd46bbf3a66ec64c41de746023447c4f8d
Kai Schwanke <kai.schwanke@gmail.com>
2019-10-27 22:27:19 +1100

    initial commit

---
+++ src/au/com/ksc/scrum/exercise1/AverageCalculation.java
@@ -0,0 +1,12 @@
+package au.com.ksc.scrum.exercise1;
+
+/**
+ * This class calculates the average of a given list of numbers
+ */
+class AverageCalculation {
+
+    static int calculateAverage(int... numbers) {
+        // implement logic here
+        return 0;
+    }
+}
---
+++ test/au/com/ksc/scrum/exercise1/AverageCalculationTest.java
@@ -0,0 +1,24 @@
+package au.com.ksc.scrum.exercise1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AverageCalculationTest {
+
+    @Test
+    public void testCalculateAverage() {
+
+        int calculatedAverage = AverageCalculation.calculateAverage(1, 2, 3);
+        assertEquals(2, calculatedAverage);
+
+        calculatedAverage = AverageCalculation.calculateAverage(5, 5, 5, 5);
+        assertEquals(5, calculatedAverage);
+
+        calculatedAverage = AverageCalculation.calculateAverage(99);
+        assertEquals(99, calculatedAverage);
+
+
+    }
+
+}
